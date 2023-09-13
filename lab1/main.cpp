9e97fe2992501fa90c60d25eca06bfbc77268cff
Anyutaa <anyuta29032004@mail.ru>
2023-09-13 22:30:22 +0300

    lab 1 vvod and vivod
    Прописали структуру для ввода и вывода, проблемы с bool и выводом

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,23 +10,74 @@ struct Truba
 	bool remont;
 };
 
-struct KC {
+struct KC 
+{
 	string nazvanKC;
 	int kolvotsekh;
 	int tsekhvrabote;
 	char effect;
 };
+
+Truba vvod_truba()
+{
+	Truba tr;
+	cout << "Enter otmetky:";
+	cin >> tr.otmetka;
+	cout << "Enter dlina:";
+	cin >> tr.dlina;
+	cout << "Enter diametr:";
+	cin >> tr.diametr ;
+	cout << "Enter remont:";
+	cin >> tr.remont;
+	return tr;
+
+};
+
+KC vvod_KC()
+{
+	KC kc;
+	cout << "Enter nazvanie KC:";
+	cin >> kc.nazvanKC;
+	cout << "Enter kolvo tsekhov:";
+	cin >> kc.kolvotsekh;
+	cout << "Enter tsekh v rabote:";
+	cin >> kc.tsekhvrabote;
+	cout << "Enter effect:";
+	cin >> kc.effect;
+	return kc;
+};
+
+void Print_truba(Truba tr)
+{
+	cout << "Enter otmetky:" << tr.otmetka
+		<< "\tEnter dlina:" << tr.dlina
+		<< "\tEnter diametr:" << tr.diametr
+		<<"\tEnter remont : " << tr.remont<<endl;
+};
+
+void Print_KC(KC kc)
+{
+	cout << "Enter nazvanie KC:" << kc.nazvanKC
+		<< "\tEnter kolvo tsekhov:" << kc.kolvotsekh
+		<< "\tEnter tsekh v rabote:" << kc.tsekhvrabote
+		<< "\tEnter effect: " << kc.effect << endl;
+};
+
 int main()
 {
-	vector <Truba> truboprovod;
+	Truba tr = vvod_truba();
+	Print_truba(tr);
+	/*vector <Truba> truboprovod;
 	size_t n = 0;
 	cin >> n;
-	truboprovod.resize(n);
+	truboprovod.resize(n);*/
 
-	vector <KC> KCgroup;
+	KC kc = vvod_KC();
+	Print_KC(kc);
+	/*vector <KC> KCgroup;
 	size_t m = 0;
 	cin >> m;
-	KCgroup.resize(m);
+	KCgroup.resize(m);*/
 
 	return 0;
 }
