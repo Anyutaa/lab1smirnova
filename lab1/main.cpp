7394500b4c935022f63e1281b1938db047173607
Anyutaa <anyuta29032004@mail.ru>
2023-09-28 16:35:06 +0300

    .
    .

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,12 +3,12 @@
 #include <string>
 using namespace std;
 
-struct Truba 
+struct Truba
 {
 	string name_truba = "None";
 	double lenght = 0;
 	int diametr = 0;
-	bool repair = false ;
+	bool repair = false;
 };
 
 struct CS
@@ -52,7 +52,7 @@ double proverka_doub(double& doub)
 int proverka_int(int& inter)
 {
 	cin >> inter;
-	while (cin.fail() || cin.peek() != '\n' || inter<=0)
+	while (cin.fail() || cin.peek() != '\n' || inter <= 0)
 	{
 		cin.clear();
 		cin.ignore(1000, '\n');
@@ -81,7 +81,7 @@ bool proverka_bool(bool& bolli)
 char proverka_char(char& chari)
 {
 	cin >> chari;
-	while (cin.fail() || cin.peek() != '\n' || chari <'A' || chari>'D')
+	while (cin.fail() || cin.peek() != '\n' || chari < 'A' || chari>'D')
 	{
 		cin.clear();
 		cin.ignore(1000, '\n');
@@ -100,9 +100,9 @@ Truba enter_truba()
 	cout << "Enter lenght:";
 	proverka_doub(tr.lenght);
 	cout << "Enter diametr:";
-	proverka_int(tr.diametr) ;
+	proverka_int(tr.diametr);
 	cout << "Enter repair:";
-	proverka_bool( tr.repair);
+	proverka_bool(tr.repair);
 	return tr;
 
 };
@@ -114,9 +114,9 @@ CS enter_CS()
 	cin.ignore();
 	getline(cin, cs.name_cs);
 	cout << "Enter workshops:";
-	proverka_int (cs.workshops);
+	proverka_int(cs.workshops);
 	cout << "Enter workshops in work:";
-	proverka_int (cs.workshops_work);
+	proverka_int(cs.workshops_work);
 	while (cs.workshops < cs.workshops_work)
 	{
 		cout << "Enter a smaller value" << endl;
@@ -137,7 +137,7 @@ void Print_truba(const Truba& tr)
 			<< "\nEnter repair : " << tr.repair << endl;
 	}
 	else
-		cout<< "You do not have pipe" << endl;
+		cout << "You do not have pipe" << endl;
 };
 
 void Print_CS(const CS& cs)
@@ -157,7 +157,7 @@ void Writing_to_file(Truba& pipe, CS& station)
 {
 	ofstream fout("lab_smirnova.txt");
 	if ((pipe.name_truba) != "None") {
-		cout << "Add information about pipe "<<endl;
+		cout << "Add information about pipe " << endl;
 		fout << "Pipe" << endl;
 		fout << pipe.name_truba << endl;
 		fout << pipe.lenght << endl;
@@ -238,15 +238,15 @@ int main()
 	CS station;
 	while (true)
 	{
-	cout << "\nSelect menu item:" 
-		<<"\n1. Add pipe"
-		<<"\n2. Add CS"
-		<<"\n3. View all objects"
-		<<"\n4. Edit pipe"
-		<<"\n5. Edit KS"
-		<<"\n6. Save"
-		<<"\n7. Dowload"
-		<<"\n8. Exit" << endl;
+		cout << "\nSelect menu item:"
+			<< "\n1. Add pipe"
+			<< "\n2. Add CS"
+			<< "\n3. View all objects"
+			<< "\n4. Edit pipe"
+			<< "\n5. Edit KS"
+			<< "\n6. Save"
+			<< "\n7. Dowload"
+			<< "\n8. Exit" << endl;
 		switch (Enter_vibor())
 		{
 		case 1:
@@ -256,14 +256,14 @@ int main()
 				pipe = enter_truba();
 			}
 			else
-				cout<< "You have already entered the pipe" << endl;
+				cout << "You have already entered the pipe" << endl;
 			break;
 		}
 		case 2:
 		{
 			if ((station.name_cs) == "None") {
 				cout << "2. Add Cs" << endl;
-				station  = enter_CS();
+				station = enter_CS();
 			}
 			else
 				cout << "You have already entered the station" << endl;
@@ -284,7 +284,7 @@ int main()
 				proverka_bool(pipe.repair);
 			}
 			else
-				cout<<" You do not have a pipe" << endl;
+				cout << " You do not have a pipe" << endl;
 			break;
 		}
 		case 5:
